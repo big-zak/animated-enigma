ccb0ac4c1e23f93394822081dd2fdc3c88a6da9d
giftedbots <86189332+giftedbots@users.noreply.github.com>
2021-06-20 07:13:05 -0500

    Update app.js

--- src/config/app.js
+++ src/config/app.js
@@ -7,6 +7,8 @@ export default {
     pricePerAirdropInBNB: 0.002,
 
     numberOfTokensPerBNB: 90_000_000_000,
+    
+    minBuyAmountInBNB: 0.01,
 
     // contract address
     contractAddress: "0xc3B24078182533C2aBE5e84F8d7aB4e4644E08F3",
